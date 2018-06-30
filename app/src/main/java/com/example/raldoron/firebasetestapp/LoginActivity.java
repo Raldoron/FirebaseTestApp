97436b7891bb1f32dd9c49d5d50b4bb1e47c2ce1
Алексей Кирсанов <Raldoron@MacBook-Air-Aleksej.local>
2018-06-30 16:51:59 +0300

    Login: fix login (empty quote list after login)

--- app/src/main/java/com/example/raldoron/firebasetestapp/LoginActivity.java
+++ app/src/main/java/com/example/raldoron/firebasetestapp/LoginActivity.java
@@ -2,7 +2,6 @@ package com.example.raldoron.firebasetestapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -12,17 +11,14 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.Profile;
-import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 
-/**
+/*
  * Created by Raldoron on 23.05.17.
  */
 
@@ -31,8 +27,6 @@ public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "FacebookLogin";
 
     private CallbackManager callbackManager;
-    private LoginButton loginButton;
-    private ProfileTracker profileTracker;
 
     private FirebaseAuth firebaseAuth;
 
@@ -41,17 +35,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        FirebaseApp.initializeApp(this);
         firebaseAuth = FirebaseAuth.getInstance();
 
-        profileTracker = new ProfileTracker() {
-            @Override
-            protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
-                startMainActivity(currentProfile);
-            }
-        };
-        profileTracker.startTracking();
-
-        loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginButton loginButton = findViewById(R.id.login_button);
         loginButton.setReadPermissions("email", "public_profile");
         callbackManager = CallbackManager.Factory.create();
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
@@ -75,41 +62,33 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+        startMainActivity();
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        startMainActivity(Profile.getCurrentProfile());
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        profileTracker.stopTracking();
-    }
-
     private void handleFacebookAccessToken(AccessToken accessToken){
         AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
         firebaseAuth.signInWithCredential(credential)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            Toast.makeText(LoginActivity.this, "Success authentication.", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
-                        }
+                .addOnCompleteListener(LoginActivity.this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Success authentication.", Toast.LENGTH_SHORT).show();
+                        startMainActivity();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
-    private void startMainActivity(Profile profile){
-        if(profile != null){
+    private void startMainActivity(){
+        Profile profile = Profile.getCurrentProfile();
+        if (profile != null && firebaseAuth.getCurrentUser() != null) {
             Intent intent = new Intent(this, MainActivity.class);
             intent.putExtra("profile", profile);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
